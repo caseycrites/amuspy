ac0ab262580b4ae4684c14e76ee3d663515a3641
Casey Crites <casey@bighorn.local>
2013-03-30 15:43:27 -0700

    Replaced optparse with argparse.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='amuspy',
-    version='0.1.0',
+    version='0.1.1',
     author='Casey W Crites',
     author_email='crites.casey@gmail.com',
     packages=['amuspy'],
